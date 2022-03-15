88ff4d9f5c9ba9251ed472c6537057a7f4a7f844
Jeremy Yearwood <jeremyyearwood99@gmail.com>
2022-03-15 12:54:18 -0400

    Add project files.

---
+++ AutoGrid/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System.Diagnostics.CodeAnalysis;
+using System.Runtime.InteropServices;
+using System.Windows;
+using System.Windows.Markup;
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+[assembly: ThemeInfo(ResourceDictionaryLocation.None, ResourceDictionaryLocation.SourceAssembly)]
+
+[assembly: SuppressMessage("Microsoft.Performance", "CA1824:MarkAssembliesWithNeutralResourcesLanguage")]
+[assembly: System.Resources.NeutralResourcesLanguage("en", System.Resources.UltimateResourceFallbackLocation.MainAssembly)]
+
+[assembly: XmlnsPrefix(@"http://schemas.spicytaco.io/", "st")]
+[assembly: XmlnsDefinition(@"http://schemas.spicytaco.io/", "AutoGrid")]
